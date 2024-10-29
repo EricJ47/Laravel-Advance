d812fdf2bebe422a84fd1a6dfabe19c36150aacc
Eric Jamal <ericjamal06@gmail.com>
2024-10-29 11:32:03 +0700

    added intervention image package

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,9 @@ use App\DataTables\UsersDataTable;
 use App\Http\Controllers\ProfileController;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
-
+use Intervention\Image\ImageManager;
+// use Intervention\Image\Drivers\Imagick\Driver;
+use Intervention\Image\Laravel\Facades\Image;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -24,7 +26,23 @@ Route::get('/dashboard', function (UsersDataTable $dataTable) {
     return $dataTable->render('dashboard', ['dataTable' => $dataTable]);
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/image', function () {
+    $image = ImageManager::imagick()->read('badass.jpg');
 
+    // $image->blur(50);
+    $image->greyscale();
+
+    // $image->crop(2000,600);
+    // $image->flip();
+    // $image->resize(300, 200);
+    $image->save('badass4.jpg'); 
+    // return $image->response(); this no longer works on intervention v3
+    
+});
+
+// Route::post('/upload', function (Request $request) {
+//     $image = Image::read($request->file('image'));
+// });
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
