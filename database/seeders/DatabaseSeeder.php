f1476ed176b1d46f01b8f1f6cafa0b638bbc4369
Eric Jamal <ericjamal06@gmail.com>
2024-10-31 15:09:16 +0700

    added laravel spatie and socialite packages

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,13 +12,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::factory(10)->create();
+        // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
 
-        \App\Models\Product::factory(5)->create();
+        // \App\Models\Product::factory(5)->create();
+
+        \App\Models\Post::factory(30)->create();
     }
 }
